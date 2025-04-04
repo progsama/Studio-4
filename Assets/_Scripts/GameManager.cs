3fe0bab653598d1dd6ac8662c5f7be51e3dd2980
progsama <imtiaznasif@gmail.com>
2025-04-03 23:10:46 -0700

    Implemented animation coin counter

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -5,6 +5,7 @@ public class GameManager : SingletonMonoBehavior<GameManager>
 {
     [SerializeField] private int score = 0;
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private CoinCounterUI coinCounter;
 
     protected override void Awake()
     {
@@ -16,6 +17,6 @@ public class GameManager : SingletonMonoBehavior<GameManager>
     public void IncreaseScore()
     {
         score++;
-        scoreText.text = $"Score: {score}";
+        coinCounter.UpdateScore(score);
     }
 }
